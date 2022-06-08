a37e72b602626eb543c04889c5fb8a74d934063a
Stanislau Khamiankou <inha.chubarava@leverx.com>
2022-06-08 06:12:18 +0300

    fix route and table users

--- src/feature/UserList/UserList.tsx
+++ src/feature/UserList/UserList.tsx
@@ -1,4 +1,5 @@
 import {
+  ColumnsType,
   ColumnType,
   FilterConfirmProps,
   SorterResult,
@@ -16,40 +17,89 @@ import { IUser, IUserState } from '../../store/user/type'
 import '../UserList/styles.scss'
 import { loadUsersAsync } from '../../store/user/usersThunk'
 import { useAppDispatch, useAppSelector } from '../../hooks'
-import { clearUsers, setUsers } from '../../store/user/userSlice'
+import { setUsers } from '../../store/user/userSlice'
+import { getDateFormat } from '../../helper/helper'
 
 export const UserList: React.FC = () => {
   const dispatch = useAppDispatch()
   const { user } = useAppSelector((state) => state.user)
   const [value, setValue] = useState('')
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const currValue = e.target.value
+    if (currValue === '') {
+      setValue('')
+      dispatch(setUsers(user))
+    } else {
+      setValue(currValue)
+      const filteredData = user.filter((entry: IUser) =>
+        entry.user_name.includes(currValue),
+      )
+      dispatch(setUsers(filteredData))
+    }
+  }
+
   const FilterByNameInput = (
     <Input
       placeholder="Search user name"
       value={value}
-      onChange={(e) => {
-        const currValue = e.target.value
-        if (currValue === '') {
-          setValue('')
-          dispatch(setUsers(user))
-        } else {
-          setValue(currValue)
-          const filteredData = user.filter((entry: IUser) =>
-            entry.user_name.includes(currValue),
-          )
-          dispatch(setUsers(filteredData))
-        }
-      }}
+      onChange={(e) => onChange(e)}
     />
   )
 
-  const sortColumnIn = (column_field: IUser) => {
-    if (typeof column_field === 'string') {
-      return (a: IUser, b: IUser) => a.first_name.localeCompare(b.first_name)
-    } else {
-      return (a: IUser, b: IUser) => a.age - b.age
-    }
-  }
+  const columns: ColumnsType<IUser> = [
+    {
+      title: 'User name',
+      children: [
+        {
+          title: FilterByNameInput,
+          dataIndex: 'user_name',
+          key: 'user_name',
+        },
+      ],
+    },
+    {
+      title: 'Full name',
+      children: [
+        {
+          title: 'First Name',
+          dataIndex: 'first_name',
+          key: 'first_name',
+          sorter: (a, b: IUser) => a.first_name.localeCompare(b.first_name),
+        },
+        {
+          title: 'Last Name',
+          dataIndex: 'last_name',
+          key: 'last_name',
+          sorter: (a, b: IUser) => a.last_name.localeCompare(b.last_name),
+        },
+      ],
+    },
+    {
+      title: 'Role',
+      dataIndex: 'role',
+      key: 'role',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      sorter: (a, b: IUser) => a.age - b.age,
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Created at',
+      dataIndex: 'created_at',
+      key: 'created_at',
+      defaultSortOrder: 'descend',
+      sorter: (a, b: IUser) => a.created_at.localeCompare(b.created_at),
+      render: (date: string) => getDateFormat(date),
+    },
+  ]
 
   useEffect(() => {
     dispatch(loadUsersAsync())
@@ -61,48 +111,8 @@ export const UserList: React.FC = () => {
         User List: {user.length}
       </Typography>
       <div className="user-list">
-        <Table dataSource={user} size="middle" bordered>
-          <ColumnGroup title="User name">
-            <Column
-              title={FilterByNameInput}
-              dataIndex="user_name"
-              key="user_name"
-            />
-          </ColumnGroup>
-          <ColumnGroup title="Full name">
-            <Column
-              title="First Name"
-              dataIndex="first_name"
-              key="first_name"
-              sorter={(a, b: IUser) => a.first_name.localeCompare(b.first_name)}
-            />
-            <Column
-              title="Last Name"
-              dataIndex="last_name"
-              key="last_name"
-              sorter={(a, b: IUser) => a.last_name.localeCompare(b.last_name)}
-            />
-          </ColumnGroup>
-          <Column title="Role" dataIndex="role" key="role" />
-          <Column
-            title="Age"
-            dataIndex="age"
-            key="age"
-            sorter={(a, b: IUser) => a.age - b.age}
-          />
-          <Column title="Email" dataIndex="email" key="email" />
-          <Column
-            title="Created at"
-            dataIndex="created_at"
-            key="created_at"
-            sorter={(a, b: IUser) => a.created_at.localeCompare(b.created_at)}
-            render={(created_at) =>
-              `${moment(created_at).format('DD-MM-YYYY')}`
-            }
-          />
-        </Table>
+        <Table dataSource={user} size="middle" columns={columns} bordered />
       </div>
-      {/* <div>{users}</div> */}
     </div>
   )
 }

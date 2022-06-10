0e9bf5df9b38a26e3f8cad5d6a5677a7d4412054
Yulia <yuliana.kliapitskaya@leverx.com>
2022-06-10 12:42:05 +0300

    delete folders

--- src/helper/helper.ts
+++
@@ -1,3 +0,0 @@
-import moment from 'moment'
-
-export const getDateFormat = (date: string) => moment(date).format('DD-MM-YYYY')
--- src/services/services.ts
+++
@@ -1,8 +0,0 @@
-import apiClient from '../helper/api'
-import { IUser } from '../store/user/type'
-
-class UsersService {
-  getAllUsers = () => apiClient().get('users')
-}
-
-export default new UsersService()

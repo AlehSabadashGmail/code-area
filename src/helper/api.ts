c35f781bedeb4878ef30c369f92f11d8b1ca2c5f
Yulia <yuliana.kliapitskaya@leverx.com>
2022-06-16 19:14:07 +0300

    fix path

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -3,13 +3,6 @@ const apiClient = () => {
   const axiosInstance = axios.create({
     baseURL: 'https://core-area-api.herokuapp.com/',
     responseType: 'json',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization:
-        'Bearer ' +
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE0MzRmYTc4LTEwMzgtNDQwOS1hNjJjLTAzMmQ2MDU1NDM1NiIsImVtYWlsIjoiYW5nZWxpYXdvb2RhcmRAaW5zZWN0dXMuY29tIiwidXNlcl9uYW1lIjoiUVRvWWF6blF6WEpObnh1SVEiLCJwYXNzd29yZCI6IlpPcnhWVEZER1U2RUxDR01tIiwiZXhwIjoxNjU1Mzk1NDc3OTE4LCJpYXQiOjE2NTUzODgyNzc5MTh9.5yW9Wuopcg57-XLH0cm5KRk9AMbzXv09lFIlV-XTC0k',
-    },
   })
 
   return axiosInstance

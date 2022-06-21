3940bb2c7677884a32c25c0bf2e1de53f1c6b01d
Stanislau Khamiankou <stanislau.khamiankou@leverx.com>
2022-06-21 18:04:16 +0300

    fix filter

--- src/components/_templates/UserList/UserList.tsx
+++ src/components/_templates/UserList/UserList.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react'
 import { Typography } from 'antd'
 
+import { getFilteredUsers, getUserInfo } from 'src/redux/users/selectors'
 import { useAppDispatch, useAppSelector } from 'src/redux/hooks'
 import { requestUserInfo } from 'src/redux/users/action'
-import { getUserInfo } from 'src/redux/users/selecor'
+import { ModalFilter } from 'src/components/_molecules'
 import { List } from 'src/components/_organisms'
 import { USER_LIST } from 'src/constants'
 import './style.scss'
-import { getFilteredUsers, getUserInfo } from '../../../redux/users/selectors'
-import { ModalFilter } from '../../_molecules'
 
 export const UserList: React.FC = () => {
   const dispatch = useAppDispatch()

11f6538ce7b64c421b52a83bdb70e36d8b64b0c7
Yulia <yuliana.kliapitskaya@leverx.com>
2022-06-21 11:23:53 +0300

    fix request

--- src/components/_templates/constants.ts
+++ src/components/_templates/constants.ts
@@ -7,8 +7,13 @@ export type FormData = {
   last_name: string
   user_name: string
   email: string
+  address: string
+  gender: string
   age: number
+  latitude: number
+  longitude: number
   role: string
+  phone: string
   password: string
 }
 
@@ -17,7 +22,12 @@ export const initialData = (formData: FormData): IUser => ({
   last_name: formData.last_name,
   user_name: formData.user_name,
   email: formData.email,
+  gender: 'male',
+  address: 'Homel',
   age: formData.age,
+  latitude: 10,
+  longitude: 10,
   role: formData.role,
+  phone: '+3752964928363',
   password: formData.password,
 })

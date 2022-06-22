5fccd7e98089910111359a2e361a0b554c2690a1
Stanislau Khamiankou <stanislau.khamiankou@leverx.com>
2022-06-22 09:50:43 +0300

    fix delete

--- src/components/_molecules/ButtonDeleteUser/ButtonDeleteUser.tsx
+++ src/components/_molecules/ButtonDeleteUser/ButtonDeleteUser.tsx
@@ -2,9 +2,9 @@ import { DeleteOutlined } from '@ant-design/icons'
 import { Button, Popconfirm } from 'antd'
 import { ButtonDeleteUserType } from './ButtonDeleteUserType'
 
-export const ButtonDeleteUser = ({ ...props }: ButtonDeleteUserType) => {
+export const ButtonDeleteUser = ({ onConfirm }: ButtonDeleteUserType) => {
   return (
-    <Popconfirm title="Are you sure?" onConfirm={props.onConfirm}>
+    <Popconfirm title="Are you sure?" onConfirm={onConfirm}>
       <Button icon={<DeleteOutlined />} />
     </Popconfirm>
   )
